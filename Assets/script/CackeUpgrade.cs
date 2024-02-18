0ff4b5e91be43be278f67122245236cf817d55d6
StSimba <156583900+StSimba@users.noreply.github.com>
2024-02-18 15:43:01 +0200

    Julkaisukandidaatti 2

--- Assets/script/CackeUpgrade.cs
+++ Assets/script/CackeUpgrade.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using System.Collections.Generic;
+//using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class CackeUpgrade : MonoBehaviour
 {
     // Reference to the SpriteRenderer component
     private SpriteRenderer spriteRenderer;
 
+      //public TextMeshProUGUI ScoreTxt;
+
     // List of sprites to cycle through
     public List<Sprite> spriteList = new List<Sprite>();
 
     // Index to keep track of the current sprite in the list
     private int currentIndex = 0;
+
+    //public int Score;
+    
 
     void Start()
     {
@@ -28,20 +35,37 @@ public class CackeUpgrade : MonoBehaviour
         {
             Debug.LogError("Sprite list is empty! Please assign sprites.");
         }
+
     }
 
+    //public void UpdateScore(int Score)
+    //{
+    //    ScoreTxt.text = value.ToString();
+    //}
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Upgrade"))
         {
+            
             Debug.Log("Upgraded");
             
             ChangeSpriteToNext();
-            
+            //AddScore();
+
+
         }
 
     }
+
+    //public void OnDestroy()
+    //{
+    //    ScoreTxt(Score);
+    //}
+    //public void AddScore()
+    //{
+    //    Score += 100;
+    //}
     // Function to change the sprite to the next one in the list
     public void ChangeSpriteToNext()
     {
--- Assets/script/GameOver.cs
+++ Assets/script/GameOver.cs
@@ -1,31 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+
 
 public class GameOver : MonoBehaviour
+
 {
-    
-    //public GameObject die_txt;
+    public GameObject Win;
     public GameObject menu;
 
     private void Start()
     {
         //die_txt.SetActive(false);
         menu.SetActive(false);
+        Win.SetActive(false);
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Die")
         {
             EndGame();
-            //die_txt.SetActive(true);
             menu.SetActive(true);
+            
+        }
+
+        if (collision.gameObject.tag == "Finish")
+        {
+            EndGame();
+            menu.SetActive(true);
+            Win.SetActive(true);
+
         }
     }
+
+    public void OnButtonClick()
+    {
+        Application.Quit();
+    }
+
+    
 
     void EndGame()
     {
         Debug.Log("Game Over!");
+       // CackeUpgrade.UpdateScore(CackeUpgrade.Score);
+
         Time.timeScale = 0f;
     }
 }
